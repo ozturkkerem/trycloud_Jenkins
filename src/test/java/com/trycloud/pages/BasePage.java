a17f5a95dda5ea389c60fbb30482e3920ad82358
magdalenagru91 <magdalenagru91@gmail.com>
2022-03-28 14:21:22 -0400

    BasePage constructor commit

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -1,4 +1,11 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
