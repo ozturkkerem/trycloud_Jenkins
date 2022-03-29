9a80cb91e97a0bed956228be55b1a7ccf8c0e76b
magdalenagru91 <magdalenagru91@gmail.com>
2022-03-29 14:39:19 -0400

    BasePage set to abstract commit

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -3,7 +3,7 @@ package com.trycloud.pages;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
