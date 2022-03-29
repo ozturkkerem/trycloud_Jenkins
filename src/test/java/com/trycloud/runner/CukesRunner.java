09d83814765e17a09a0aac8cb58f1be5a5a4447d
magdalenagru91 <magdalenagru91@gmail.com>
2022-03-29 10:18:08 -0400

    CukesRunner fix commit

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions",
+        glue = "com/trycloud/step_definitions",
         dryRun = false,
         tags = ""
 )
